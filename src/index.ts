6c9eb723ebc489d68df5fa6c0bbcaf58195d2382
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-08-13 11:45:29 +0300

    refactor: Update index.ts to use neon, drizzle-orm, and schema

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./lib/db/schema";
 
 type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
+	DATABASE_URL: string;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+	const sql = neon(c.env.DATABASE_URL);
 
-export default app
+	const db = drizzle(sql, {
+		schema,
+	});
+
+	const authors = await db.query.authors.findMany();
+
+	return c.json(authors);
+});
+
+export default app;
